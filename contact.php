1dd7b3ff5c9d69cdaf0335a5321ec22075e1f8a5
Oladapo <ojeleyeoladapo@gmail.com>
2023-06-20 18:12:20 +0100

    Adding all my files

---
+++ contact.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="inc/contact.css">
+
+<header>
+        <a href="index.php">
+            <h1>Restuarant</h1>
+        </a>
+        <nav>
+            <ul>
+                <li><a href="index.php">HOME</a></li>
+                <li><a href="">ABOUT US</a></li>
+                <li><a href="">CHEFS</a></li>
+                <li><a href="">MENU</a></li>
+                <li><a href="contact.php">CONTACTS</a></li>
+            </ul>
+        </nav>
+</header>
+
+<div class="head">
+    <img src="inc/logo.png" alt="">
+    <h1>CONTACTS</h1>
+</div
